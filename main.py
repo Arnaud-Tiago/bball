72dfa5ab32c95acb8bbb7c5295152bb466a0e5e9
Arnaud Odet <arnaud.odet@gmail.com>
2022-12-13 19:06:12 +0100

    ready to launch VM

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from data import increment_games_df, increment_league_df
+from data import increment_games_df, increment_league_df, load_data
 from params import NB_BATCH
 
 if __name__ == '__main__':
-    for i in range(NB_BATCH):
-        print(f"Scraping batch number {i+1} out of {NB_BATCH}")
-        increment_league_df(source='cloud')
+    lldf = load_data(table_name='last_indices',provenance='cloud').set_index('table')
+    last_ind = lldf.loc['all_fiba_games','last_scraped_index']
+    while last_ind < 2172000:
+        for i in range(NB_BATCH):
+            print(f"Scraping batch number {i+1} out of {NB_BATCH}")
+            increment_games_df(source='cloud')
