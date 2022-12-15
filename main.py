a79d7377af144d289e4f9fd5958c9be71825f666
Arnaud Odet <arnaud.odet@gmail.com>
2022-12-15 10:29:43 +0100

    adding debugging cwd

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from data import increment_games_df, increment_league_df, load_data
 from params import NB_BATCH
+import os
 
 if __name__ == '__main__':
+    print(os.getcwd())
     lldf = load_data(table_name='last_indices',provenance='cloud').set_index('table')
     last_ind = lldf.loc['all_fiba_games','last_scraped_index']
     while last_ind < 2172000:
