672591360b072fc193c5d4f3f380b80a6d47c6d2
Arnaud Odet <arnaud.odet@gmail.com>
2022-12-15 17:32:29 +0100

    Modification of the main.py file so it can start in both games and leande method

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from data import increment_games_df, increment_league_df, load_data
-#from params import NB_BATCH
+from params import NB_BATCH
 import os
+
+method = 'games'
 
 if __name__ == '__main__':
     print(os.getcwd())
     lldf = load_data(table_name='last_indices',provenance='cloud').set_index('table')
-    last_ind = lldf.loc['all_fiba_games','last_scraped_index']
-    while last_ind < 2172000:
-        for i in range(NB_BATCH):
-            print(f"Scraping batch number {i+1} out of {NB_BATCH}")
-            increment_games_df(source='cloud')
+    last_ind = lldf.loc['all_fiba_games','last_scraped_index'] 
+    if method == 'games':  
+        while last_ind < 2172000:
+            lldf = load_data(table_name='last_indices',provenance='cloud').set_index('table')
+            last_ind = lldf.loc['all_fiba_games','last_scraped_index']  
+            for i in range(NB_BATCH):
+                print(f"Scraping batch number {i+1} out of {NB_BATCH}")
+                increment_games_df(source='cloud')
+    else :
+        while last_ind < 2172000:
+            lldf = load_data(table_name='last_indices',provenance='cloud').set_index('table')
+            last_ind = lldf.loc['league','last_scraped_index']  
+            for i in range(NB_BATCH):
+                print(f"Scraping batch number {i+1} out of {NB_BATCH}")
+                increment_league_df(source='cloud')        
