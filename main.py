478d9fbb0884a643304491f09a3b00610ab159c1
Arnaud Odet <arnaud.odet@gmail.com>
2022-12-16 09:24:00 +0100

    Adding an execute function to be call on the VM

--- main.py
+++ main.py
@@ -22,3 +22,23 @@ if __name__ == '__main__':
             for i in range(NB_BATCH):
                 print(f"Scraping batch number {i+1} out of {NB_BATCH}")
                 increment_league_df(source='cloud')        
+
+
+def execute():
+    print(os.getcwd())
+    lldf = load_data(table_name='last_indices',provenance='cloud').set_index('table')
+    last_ind = lldf.loc['all_fiba_games','last_scraped_index'] 
+    if method == 'games':  
+        while last_ind < 2172000:
+            lldf = load_data(table_name='last_indices',provenance='cloud').set_index('table')
+            last_ind = lldf.loc['all_fiba_games','last_scraped_index']  
+            for i in range(NB_BATCH):
+                print(f"Scraping batch number {i+1} out of {NB_BATCH}")
+                increment_games_df(source='cloud')
+    else :
+        while last_ind < 2172000:
+            lldf = load_data(table_name='last_indices',provenance='cloud').set_index('table')
+            last_ind = lldf.loc['league','last_scraped_index']  
+            for i in range(NB_BATCH):
+                print(f"Scraping batch number {i+1} out of {NB_BATCH}")
+                increment_league_df(source='cloud')  
