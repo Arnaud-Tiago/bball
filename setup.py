d4d32be39962cc10985e198bac666532d3b9a713
Arnaud Odet <arnaud.odet@gmail.com>
2022-12-16 17:05:28 +0100

    updated requirements to boot on Raspberry

--- setup.py
+++ setup.py
@@ -10,10 +10,9 @@ setup(name='basket-ball',
       description="Basket-Ball project",
       author="A.Odet",
       author_email="arnaud.odet.pro@gmail.com",
-      url="https://github.com/Arnaud-Tiago/basket-ball",
+      url="https://github.com/Arnaud-Tiago/bball",
       install_requires=requirements,
       packages=find_packages(),
-      test_suite="tests",
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
       zip_safe=False)
