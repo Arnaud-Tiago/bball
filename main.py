1aeb28e86b587df2520660ec96460b0bc0673b37
Arnaud Odet <arnaud.odet@gmail.com>
2022-12-16 17:40:14 +0100

    modifying the main.py script so it DRI

--- main.py
+++ main.py
@@ -4,26 +4,6 @@ import os
 
 method = 'games'
 
-if __name__ == '__main__':
-    print(os.getcwd())
-    lldf = load_data(table_name='last_indices',provenance='cloud').set_index('table')
-    last_ind = lldf.loc['all_fiba_games','last_scraped_index'] 
-    if method == 'games':  
-        while last_ind < 2172000:
-            lldf = load_data(table_name='last_indices',provenance='cloud').set_index('table')
-            last_ind = lldf.loc['all_fiba_games','last_scraped_index']  
-            for i in range(NB_BATCH):
-                print(f"Scraping batch number {i+1} out of {NB_BATCH}")
-                increment_games_df(source='cloud')
-    else :
-        while last_ind < 2172000:
-            lldf = load_data(table_name='last_indices',provenance='cloud').set_index('table')
-            last_ind = lldf.loc['league','last_scraped_index']  
-            for i in range(NB_BATCH):
-                print(f"Scraping batch number {i+1} out of {NB_BATCH}")
-                increment_league_df(source='cloud')        
-
-
 def execute():
     print(os.getcwd())
     lldf = load_data(table_name='last_indices',provenance='cloud').set_index('table')
@@ -42,3 +22,7 @@ def execute():
             for i in range(NB_BATCH):
                 print(f"Scraping batch number {i+1} out of {NB_BATCH}")
                 increment_league_df(source='cloud')  
+
+
+if __name__ == '__main__':
+    execute()
