cf34a5582669d467c3d807fb848e58e00b502324
Arnaud Odet <arnaud.odet@gmail.com>
2022-12-16 10:09:06 +0100

    Hard coding the params

--- params.py
+++ params.py
@@ -1,9 +1,7 @@
 import os
 
-BACTH_SIZE = int(os.environ.get("BATCH_SIZE"))
-LOCAL_DATA_PATH = os.path.expanduser(os.environ.get("LOCAL_DATA_PATH"))
-PROJECT = os.environ.get("PROJECT")
-DATASET = os.environ.get("DATASET")
-DATA_STORAGE = os.environ.get("DATA_STORAGE")
-BUCKET_NAME = os.environ.get("BUCKET_NAME")
-NB_BATCH = int(os.environ.get("NB_BATCH"))
+BACTH_SIZE = 1000
+LOCAL_DATA_PATH = "~/.basket_project/data/"
+PROJECT = 'basket-369913'
+BUCKET_NAME = "basket-ao-bucket"
+NB_BATCH = 20
